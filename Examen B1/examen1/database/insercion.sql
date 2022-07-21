83af78cc6e93c8f6306f15fd199d8b029124be5d
Fernando Ponce <fernando.p11@epn.edu.ec>
2022-07-21 07:56:06 -0500

    actualizacion

--- Examen B1/examen1/database/insercion.sql
+++ Examen B1/examen1/database/insercion.sql
@@ -5,7 +5,11 @@ values ('1','24 mayo','publica','quito','5')
 insert into escuela(id_escuela,institucion,tipo_de_institucion,direccion,ranking_)
 values ('2','montufar','publica','quito','4')
 
+insert into escuela(id_escuela,institucion,tipo_de_institucion,direccion,ranking_)
+values ('5','maurice','privada','quito','10');
 
+insert into escuela(id_escuela,institucion,tipo_de_institucion,direccion,ranking_)
+values ('4','mejia','privada','quito','1');
 
 
 */
--- Examen B1/examen1/src/pages/api/escuelas/[id].ts
+++ Examen B1/examen1/src/pages/api/escuelas/[id].ts
@@ -1,17 +1,41 @@
 import {NextApiRequest,NextApiResponse} from "next";
+import {conn} from "../../../utils/database"
 
-export default (req:NextApiRequest,res:NextApiResponse)=>{
+export default async (req:NextApiRequest,res:NextApiResponse)=>{
 
         const {method,query} = req;
-        console.log(query)
+
 
         switch (method){
                 case "GET":
-                        return res.json("getting unique task")
+                        try {
+                                const text="select * from escuela where id_escuela=$1"   ///
+                                const values=[query.id]
+                                const result =await conn.query(text,values);
+
+                                if (result.rows.left===0)
+                                        return res.status(404).json({message:"escuela no Encontrada"}) ///
+
+                                return res.json("getting unique escuela")//
+                        }catch (error:any){
+                                return res.status(500).json({message:error.message});
+                        }
+
                 case "PUT":
-                        return res.json("getting unique task")
+                        return res.json("getting unique escuela")//
                 case "DELETE":
-                        return res.json("getting unique task")
+                        try {
+                                const text="delete from escuela where id_escuela=$1 returning *"   ///
+                                const values=[query.id]
+                                const result =await conn.query(text,values);
+
+                                if (result.rowCount===0)
+                                        return res.status(404).json({message:"escuela no Encontrada"}) ///
+
+                                return res.json("eliminando unique escuela")//
+                        }catch (error:any){
+                                return res.status(500).json({message:error.message});
+                        }
                 default:
                         return res.status(400).json("methos not allowed")
 
